525398e96c77ba19c53ee6dfcfac4cacd20d132e
Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
2023-02-17 10:12:13 +0100

    Update copyright date

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019–2022 Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
+ * Copyright (C) 2019–2023 Miguel Rodríguez Pérez <miguel@det.uvigo.gal>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
